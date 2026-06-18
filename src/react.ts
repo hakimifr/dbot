1d947064d4a3e5d51fa1ee96d13178ed00161be0
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2026-06-18 23:58:20 +0800

    expand collection

--- src/react.ts
+++ src/react.ts
@@ -4,6 +4,10 @@ const reactMessageCallback = async (m: Message): Promise<void> => {
   const reactions: Record<string, string> = {
     woof: "🐶",
     oink: "🐷",
+    quack: "🦆",
+    hoot: "🦉",
+    hiss: "🐍",
+    rawr: "🦖",
   };
   const r = reactions[m.content.trim().toLowerCase()];
   if (r) m.react(r);
